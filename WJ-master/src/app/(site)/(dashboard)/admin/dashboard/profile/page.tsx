ad76fbd93c4d27f9f0bb61649e310032af5d9164
abhijeet <abhijeet@eazotel.com>
2024-04-22 22:27:57 +0530

    up

---
+++ WJ-master/src/app/(site)/(dashboard)/admin/dashboard/profile/page.tsx
@@ -0,0 +1,12 @@
+// "use client"
+
+// import { useState } from "react"
+
+const page = () => {
+    // const [user, setuser] = useState();
+    return (
+        <div>kjxhgkdhfkshhfkhsdfjdhgshghjProfile</div>
+    )
+}
+
+export default page
